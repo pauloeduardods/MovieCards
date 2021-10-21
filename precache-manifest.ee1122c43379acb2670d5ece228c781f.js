818040a43cb6cc9ba75145a4c25c20f9be194de1
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-10-21 18:46:05 -0300

    Updates

---
+++ precache-manifest.ee1122c43379acb2670d5ece228c781f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032818eccd6fb34425dfdd65e13b356f",
+    "url": "/MovieCards/index.html"
+  },
+  {
+    "revision": "98bcffa64fa57eae9462",
+    "url": "/MovieCards/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "52634c165b0d42662954",
+    "url": "/MovieCards/static/js/2.a6e0ab81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MovieCards/static/js/2.a6e0ab81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98bcffa64fa57eae9462",
+    "url": "/MovieCards/static/js/main.de96908a.chunk.js"
+  },
+  {
+    "revision": "243b2fcccd19bb139ae5",
+    "url": "/MovieCards/static/js/runtime-main.d9ae80a2.js"
+  }
+]);
